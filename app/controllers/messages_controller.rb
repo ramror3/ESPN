3caef6e79fcfeada5078e89ba54f1e768f2cfbd2
ramror3 <ramesh.sabbanis3@gmail.com>
2019-02-26 10:28:48 +0530

    add styles to the application

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 	before_action :find_message, except: [:index, :new, :create]
 
 	def index
